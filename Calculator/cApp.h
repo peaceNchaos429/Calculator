51aecb1da474ae100ad8e981c28eba9e9761bd64
Briana Buttone <BKButtone@student.fullsail.edu>
2022-05-13 17:17:20 -0400

    window

--- Calculator/cApp.h
+++ Calculator/cApp.h
@@ -1,5 +1,18 @@
 #pragma once
-class cApp
+
+#include"wx/wx.h"
+#include "cMain.h"
+class cApp : public wxApp
 {
+public:
+	cApp();
+	~cApp();
+
+private:
+	cMain* m_frame1 = nullptr;
+public:
+	virtual bool OnInit();
+
+	
 };
 
---
+++ Calculator/cMain.cpp
@@ -0,0 +1,13 @@
+#include "cMain.h"
+
+
+cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(30,30), wxSize(800,600))
+{
+
+}
+
+
+cMain::~cMain()
+{
+
+}
